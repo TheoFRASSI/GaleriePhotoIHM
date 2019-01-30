2e05d3694e9f7e2149a1e61f29992cbc24d2c351
luca <luca.lacaille.iut@gmail.com>
2019-01-30 11:23:30 +0100

    Maj branch image_win_00

--- imagewindow.h
+++ imagewindow.h
@@ -3,12 +3,12 @@
 
 #include <QObject>
 #include <QWidget>
-#include "ui_accueilwindow.h"
+#include "ui_imagewindow.h"
 
 #include <map>
 
 
-class ImageWindow : public QWidget, private Ui::AccueilWindow
+class ImageWindow : public QWidget, private Ui::ImageWindow
 {
     Q_OBJECT
 public:
